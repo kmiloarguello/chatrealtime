30b1d045e8b3860c29d8a16daf6111a36cd0d9b4
Camilo Arguello <camilo.arguello.rincon@gmail.com>
2017-11-14 15:50:58 -0500

    Icons and build failed

--- app/src/main/java/xyz/camiloarguello/chatrealtime/MainActivity.java
+++ app/src/main/java/xyz/camiloarguello/chatrealtime/MainActivity.java
@@ -1,13 +1,25 @@
 package xyz.camiloarguello.chatrealtime;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int SIGN_IN_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+
+
+
 }
